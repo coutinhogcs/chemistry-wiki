6a8f842afe15319671108fb946ff139d7c8eeb7d
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-07-07 20:49:21 -0300

    Creation of Human Practice screen

---
+++ src/app/human-practice/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Human Practice - O Siris Project',
+  description: 'Chemistry Project'
+}
+
+export default function HumanPratice(){
+    return(
+        <div><h1>Pagina Human Pratice</h1></div>
+    )
+}
