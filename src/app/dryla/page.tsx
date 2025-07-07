3a26a634df69d8677fc5d214056eaa13a37130e5
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-07-07 20:48:59 -0300

    feat: Creation of Dry Lab screen

---
+++ src/app/dryla/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Dryla - O Siris Project',
+  description: 'Chemistry Project'
+}
+
+export default function Dryla(){
+    return(
+        <div><h1>Pagina Dryla</h1></div>
+    )
+}
