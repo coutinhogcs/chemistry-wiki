f4ae29a30781fee51c47b218082532a03ff63c51
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-07-07 20:50:01 -0300

    feat: Creation of Wet Lab screen

---
+++ src/app/wetlab/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Wetlab - O Siris Project',
+  description: 'Chemistry Project'
+}
+
+export default function WetLab(){
+    return(
+        <div><h1>Pagina WetLab</h1></div>
+    )
+}
